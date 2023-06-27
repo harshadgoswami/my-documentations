8838b6c42a7ed8e02141a2e956ef7ec0889b86ad
Harshad Goswami <h.goswami@vbittech.com>
2023-06-27 20:20:24 +0530

    email template added

---
+++ email-tempaltes/kyc-request-email-template.ts
@@ -0,0 +1,49 @@
+Certainly! Here's an HTML responsive email template for the KYC verification request notification on approval to the customer:
+
+    ```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KYC Verification Approval Notification</title>
+    <style>
+        /* Add your custom styles here */
+    </style>
+</head>
+<body>
+    <table cellpadding="0" cellspacing="0" border="0" align="center" width="100%" style="max-width: 600px;">
+        <tr>
+            <td>
+                <table cellpadding="0" cellspacing="0" border="0" align="center" width="100%" style="border-collapse: collapse;">
+                    <tr>
+                        <td style="background-color: #f5f5f5; padding: 20px; text-align: center;">
+                            <h2 style="color: #333333;">KYC Verification Approval Notification</h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 20px;">
+                            <p>Dear [Customer's Name],</p>
+                            <p>We are delighted to inform you that your KYC (Know Your Customer) verification process has been successfully completed and approved. On behalf of [Company Name], we would like to thank you for your cooperation and patience throughout the verification procedure.</p>
+                            <p>The KYC verification is an essential step in ensuring the security and integrity of our services. By completing this process, you have helped us maintain a safe and trustworthy environment for all our customers.</p>
+                            <p>Now that your KYC verification has been approved, you can enjoy the full benefits and services provided by [Company Name]. We value your trust in our organization and remain committed to providing you with exceptional service and support.</p>
+                            <p>Should you have any questions or require further assistance, please do not hesitate to reach out to our dedicated customer support team. We are here to assist you in any way we can.</p>
+                            <p>Once again, thank you for choosing [Company Name]. We appreciate your cooperation and look forward to serving you.</p>
+                            <br>
+                            <p>Best regards,</p>
+                            <p>[Your Name]</p>
+                            <p>[Your Position]</p>
+                            <p>[Company Name]</p>
+                            <p>[Contact Information]</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+```
+
+Feel free to customize the styles, colors, and content within the `<style>` and `<body>` sections to match your branding and preferences.
